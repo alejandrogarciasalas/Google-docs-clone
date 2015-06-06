f4a391e6bfefe78f9451bf93667837e6157ef94f
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2015-06-05 23:51:23 -0500

    added a really basic and somewhat buggy control sytem for connected users

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
+var randomUserNames = ['Anonymous Nyan Cat', "Anonymous Llama", "Anonymous Troll", "Anonymous Doge"];
+var randomUsername;
+var connectedUsers = [];
+
 var usersConnected = [];
 io.on('connection', function(socket){
   console.log('a user connected');
+	randomUsername = randomUserNames[Math.floor(Math.random()*randomUserNames.length)];
+	connectedUsers.push(randomUsername);
+  console.log(connectedUsers);
 
+  io.emit('join', connectedUsers);
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
-
+    connectedUsers.pop();
+    console.log(connectedUsers);
   });
 
 
